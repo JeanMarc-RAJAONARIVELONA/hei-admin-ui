bb82b466262d4e1faa06c7de9158185650802dbe
RickaPrincy <rckprincy@gmail.com>
2024-01-05 12:30:26 +0300

    chore: remove show one

--- src/operations/attendance/list/AttendanceList.js
+++ src/operations/attendance/list/AttendanceList.js
@@ -1,30 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { FunctionField, TextField } from 'react-admin'
 import { formatDate } from '../../../ui/utils'
 import { AttendanceActions } from './AttendanceActions'
-import ShowOne from './ShowOne'
 import { HaList } from '../../../ui/haList'
 import { PanToolAltOutlined } from '@mui/icons-material'
 
 export function AttendanceList() {
-  const [showOne, setShowOne] = useState({ status: false, record: null })
-  const showRecord = (id, ressource, record) => setShowOne({ status: true, record })
-
   return (
-    <>
-      <HaList
-        title='Listes des pointages'
-        icon={ <PanToolAltOutlined /> }
-        mainSearch={{source:'student_key_word', label: 'Étudiant'}}
-        actions={<AttendanceActions />}
-        resource='attendance'
-      >
-        <TextField source='student.ref' label='Référence' />
-        <TextField source='student.first_name' label='Prénom·s' />
-        <TextField source='place' label='Lieu' />
-        <FunctionField label='Heure' render={record => formatDate(record.created_at) || 'Absent' }/>
-      </HaList>
-      <ShowOne showOne={showOne} setShowOne={setShowOne} />
-    </>
+    <HaList
+      title='Listes des pointages'
+      icon={ <PanToolAltOutlined /> }
+      mainSearch={{source:'student_key_word', label: 'Étudiant'}}
+      actions={<AttendanceActions />}
+      resource='attendance'
+    >
+      <TextField source='student.ref' label='Référence' />
+      <TextField source='student.first_name' label='Prénom·s' />
+      <TextField source='place' label='Lieu' />
+      <FunctionField label='Heure' render={record => formatDate(record.created_at) || 'Absent' }/>
+    </HaList>
   )
 }
